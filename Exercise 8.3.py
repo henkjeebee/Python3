f658c1e045f8682d63d977678d844748e1603b4d
henkjeebee <62592737+henkjeebee@users.noreply.github.com>
2020-04-30 10:46:23 +0200

    Fixed code on line 5

--- Exercise 8.3.py
+++ Exercise 8.3.py
@@ -2,6 +2,6 @@ fhand = open('mbox-short.txt')
 count = 0
 for line in fhand:
     words = line.split()
-    if words[0] != 'From' or line == '':
+     if line == '' or if words[0] != 'From':
         print('Debug:', words)
     #print(words[2])
